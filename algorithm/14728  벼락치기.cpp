7139de891af0479824562b16ab3b907694e57ab8
leejieun94 <38100269+leejieun94@users.noreply.github.com>
2020-03-11 23:12:42 +0900

    Use knapsack

--- algorithm/14728  벼락치기.cpp
+++ algorithm/14728  벼락치기.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int dp[101][10001] = { 0, };
-int n, t;
-vector<pair<int, int>> input;
-
-int knapsack(int index, int capacity) {
-	if (index == n) return 0;
-	int max_value = dp[index][capacity];
-	if (max_value != 0)return max_value;
-	if (input[index].first <= capacity) {
-		max_value = knapsack(index + 1, capacity - input[index].first) + input[index].second;
-	}
-	max_value = max(max_value, knapsack(index + 1, capacity));
-	return max_value;
-}
+int dp[10001] = { 0, };
 
 int main() {
-	int k, s;
-	int max_value = 0;
+	int n, t, k, s;
 	cin >> n >> t;
-
-	for (int a = 0; a < n; a++) {
+	while (n--) {
 		cin >> k >> s;
-		input.push_back(make_pair(k, s));
+		for (int i = t; i >= k; i--) {
+			dp[i] = max(dp[i], dp[i - k] + s);
+		}
 	}
-	cout << knapsack(0, t) << "\n";
+	cout << dp[t] << "\n";
 	return 0;
 }
