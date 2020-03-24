95b89470a2be8632b29c03eab5bb700848cb8296
leejieun94 <38100269+leejieun94@users.noreply.github.com>
2020-03-24 19:55:01 +0900

    Update lev2 전화번호 목록.cpp
    정렬을 한 뒤에 이전 배열의 길이만큼 hash table에 있는지 탐색 한다.
    1) 있을 경우 return false
    2) 없을 경우 hash table에 key저장, value에 1저장.

--- algorithm/lev2 전화번호 목록.cpp
+++ algorithm/lev2 전화번호 목록.cpp
@@ -5,31 +5,30 @@
 
 using namespace std;
 
-//배열 중 가장 짧은 길이를 가진 것의 길이만큼만 value로 넣는다
 bool solution(vector<string> phone_book) {
 	bool answer = true;
-	int len = 20;
-	for (int j = 0; j < phone_book.size(); j++) {
-		if (len > phone_book[j].size()) len = phone_book[j].size();
-	}
+	sort(phone_book.begin(), phone_book.end());
+	int len = phone_book[0].size();
 	unordered_map<string, int> um;
 
 	for (int i = 0; i < phone_book.size(); i++) {
 		string tmp = "";
 		tmp.append(phone_book[i], 0, len);
-		um[tmp]++;
-		if (um[tmp] > 1) return false;
+		if (um[tmp] == 1) return false;
+		else {
+			um[phone_book[i]]++;
+		}
 	}
-
 	return answer;
+
 }
 
 void main() {
 
 	vector<string> v;
-	v.push_back("0123246");
-	v.push_back("1195524421");
-	v.push_back("23456");
+	v.push_back("111113");
+	v.push_back("1112");
+	v.push_back("12");
 	v.push_back("119");
 	solution(v);
 }
