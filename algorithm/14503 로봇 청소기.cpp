06820a4c938984ee62f32a5686fbd646341fad1e
leejieun94 <38100269+leejieun94@users.noreply.github.com>
2020-10-08 01:19:49 +0900

    Update 14503 로봇 청소기.cpp
    문제 이해를 잘못한 것 같다. 아직도 못고쳤다.

--- algorithm/14503 로봇 청소기.cpp
+++ algorithm/14503 로봇 청소기.cpp
@@ -1,67 +1,81 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int n, m;
-int r, c, dir;
-vector<vector<int>> v; //0-청소필요 1-벽 2-청소됨
-int answer = 0;
+int romap[51][51]; //0-청소필요 1-벽 2-청소됨
+int answer = 0; //청소한 구역
 
+
+// 0:북 1:동 2:남 3:서
+int bx[4] = { 1,0,-1,0 };
+int by[4] = { 0,1,0,-1 };
+
+
+// 0:서 1:남 2:동 3:북 : left
 int dx[4] = { 0,-1,0,1 };
 int dy[4] = { -1,0,1,0 };
-int bx[4] = { 1,0,-1,1 };
-int by[4] = { 0,-1,0,1 };
 
+// rear 남 서 북 동
+int rx[4] = { -1,0,1,0 };
+int ry[4] = { 0,-1,0,1 };
 
+int d[4] = { 3,2,1,0 };
 
+struct {
+	int r, c, dir;
+	int dcnt = 0; //회전 수 
+}robo;
 
-//return 0 는 작동을 멈출조건
-void dfs(int x, int y, int d) {
-	if (v[x][y] == 1) {
-		return;
-	}
-	if (v[x][y]==0) {
-		v[x][y] = 2;
+void dfs() {
+	if (romap[robo.r][robo.c] == 0) {
+		romap[robo.r][robo.c] = 2;
 		answer++;
 	}
-	for (int i = d; i > d-4 ; i--) {
-		int nx = x + dx[(i + 4) % 4];
-		int ny = y + dy[(i + 4) % 4];
-		if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-			if (v[nx][ny] == 0) {
-				dfs(nx, ny, (i + 3) % 4);
-				return ;
-			}
+	robo.dcnt = 0;
+	for (int i = 0; i < 4; i++) {
+		//왼쪽 방향
+		int nx = robo.r + dx[robo.dir];
+		int ny = robo.c + dy[robo.dir];
 
+		//왼쪽 방향에 청소할 곳 있으면
+		if (romap[nx][ny] == 0) {
+			//방향 바꾸기
+			robo.dir = d[robo.dir];
+			//한 칸 이동
+			robo.r = nx;
+			robo.c = ny;
+			dfs();
+			return;
+		}
+		else { //왼쪽 방향에 청소할 곳 없으면
+			//방향 바꾸기
+			robo.dir = d[robo.dir];
+			robo.dcnt++;
 		}
 	}
-	int bnx = x + bx[d];
-	int bny = y + by[d];
-	/*if (bnx >= 0 && bnx < n && bny >= 0 && bny < m) {
-		if (v[bnx][bny] == 2) {
-			dfs(bnx, bny, d);
+	if (robo.dcnt == 4) {
+		int nx = robo.r + rx[robo.dir];
+		int ny = robo.c + ry[robo.dir];
+		if (romap[nx][ny] == 1) return;
+		else {
+			robo.r = nx;
+			robo.c = ny;
+			dfs();
 		}
-	}*/
-	dfs(bnx, bny, d);
-
-	/*return 0;
-*/
+	}
 }
 
 int main() {
 	cin >> n >> m;
-	cin >> r >> c >> dir;
-	v.resize(n);
+	cin >> robo.r >> robo.c >> robo.dir;
+
 	for (int i = 0; i < n; i++) {
-		vector<int> vy;
-		vy.resize(m);
 		for (int j = 0; j < m; j++) {
-			cin >> vy[j];
+			cin >> romap[i][j];
 		}
-		v[i] = vy;
 	}
 
-	dfs(r, c, dir);
+	dfs();
 	cout << answer;
 	
 	system("pause");
