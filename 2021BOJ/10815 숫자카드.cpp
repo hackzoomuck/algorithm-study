932d1aae0186c0917b3b1167c0981e8b9c48b3b0
leejieun <38100269+hackzoomuck@users.noreply.github.com>
2021-02-06 19:15:17 +0900

    이분탐색 풀이
    입출력 시간 문제로 
    cin.tie(0);
    ios_base::sync_with_stdio(false);
    추가하면 해결된다.

--- 2021BOJ/10815 숫자카드.cpp
+++ 2021BOJ/10815 숫자카드.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include<vector>
 using namespace std;
-int n,m,arr1[500001],y;
+
+int binarySearch(vector<int> &tmp, int num){
+    int left = 0;
+    int right = tmp.size()-1;
+    while(left<=right){
+        int mid = (left+right)/2;
+        if(tmp[mid]==num){
+            return 1;
+        }
+        else if(tmp[mid]<num){
+            left = mid+1;
+        }
+        else{
+            right = mid-1;
+        }
+    }
+    return 0;
+}
+
 int main() {
-
-	vector<int> vt;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr1[i]);
-		vt.push_back(arr1[i]);
-	}
-	sort(vt.begin(), vt.end());
-	scanf("%d", &m);
-	for (int i = 0; i < m; i++) {
-		scanf("%d", &y);
-		printf("%d ", binary_search(vt.begin(), vt.end(), y));
-
-	}
-
+cin.tie(0);
+ios_base::sync_with_stdio(false);
+    int n, m, x;
+    vector<int> v;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+    cin>>m;
+    for(int i=0;i<m;i++){
+        cin>>x;
+        cout<< binarySearch(v, x) <<" ";
+    }
+    
+    return 0;
 }
